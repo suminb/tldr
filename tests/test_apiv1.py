e3d5ba95e32ff795ed9130ee0580b1e822003f10
Sumin Byeon <suminb@gmail.com>
2016-12-29 13:15:57 +0000

    Fix broken test cases

--- tests/test_apiv1.py
+++ tests/test_apiv1.py
@@ -1,13 +1,9 @@
 import pytest
 
-from tldr.apiv1 import __summarize_url__
-
 
 @pytest.mark.parametrize('content_type', ['application/json', 'text/plain'])
 def test_summarize_url(testapp, content_type):
     url = 'https://github.com/suminb/finance'
-    data = __summarize_url__(url)
-
     headers = {'Accept': content_type}
     data = {'url': url}
     resp = testapp.post('/api/v1/summarize-url', data=data, headers=headers)
